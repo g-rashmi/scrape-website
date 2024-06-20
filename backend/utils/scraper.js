5b580f3621c419d0e9ac05de6662418023bca235
Rashmi Gupta <110086481+g-rashmi@users.noreply.github.com>
2024-06-20 21:39:17 +0530

    Update scraper.js

--- backend/utils/scraper.js
+++ backend/utils/scraper.js
@@ -30,9 +30,7 @@ const cleanEmail = (email) => {
 
 exports.scrape = async (url) => {
   const browser = await puppeteer.launch({ headless: true,args: ['--no-sandbox', '--disable-setuid-sandbox','--single-process','--no-zygote'] , executablePath:
-    process.env.NODE_ENV === "production"
-      ? process.env.PUPPETEER_EXECUTABLE_PATH
-      : puppeteer.executablePath(),
+   puppeteer.executablePath(),
  });
   try {
   
