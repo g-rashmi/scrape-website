e861e3e5a7eae43bd80e0f19641487377b7b74d6
rashmi <rashmi.gupta7018@gmail>
2025-02-20 12:30:26 +0530

    some-changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+\import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    host: "0.0.0.0",  // This makes the Vite dev server accessible from any IP address
+    port: 5173,       // Port your app is running on (you can change it if necessary)
+  },
+});
